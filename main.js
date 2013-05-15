7afdc993866ff60324d4940697d35a0a30a77900
olls <faircliff.oliver@gmail.com>
2013-05-15 19:08:51 +0100

    Loading from file now works

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 function submitPost() {
+	var newPost = $('#newPost').val().replace(/\n+/g,"<br>") + '\n';
+	$('#newPost').val('');
 	$.ajax({
 		url: 'savePost.php',
 		type: 'post',
-		data: {postContent: $('#newPost').val()},
+		data: {postContent: newPost},
 		success: function (post){
-			alert(post);
+			receivePosts();
 		}
 	});
 }
+
+function receivePosts() {
+	$.get('posts.txt', function(posts) {
+		var splitPosts = posts.split('\n');
+		$('#board ol').html(function () {
+			if (splitPosts.length < defaultLoad) {
+				var no = splitPosts.length;
+			} else {
+				var no = defaultLoad;
+			};
+			
+			var out = '';
+			for (var i = splitPosts.length-2; i >= (splitPosts.length - no); i--) {
+				out = out + '<li>' + splitPosts[i] + '</li>';
+			};
+			return out;
+		});
+	});
+}
+
+function resizeElm() {
+	$('#post textarea').css('width', $('#post').width()-43);
+}
+
+var defaultLoad = 10
+
+$(document).ready(function () {
+	resizeElm();
+	$(window).resize(resizeElm);
+	receivePosts();
+
+	setInterval(receivePosts, 2000);
+});
--- savePost.php
+++ savePost.php
@@ -1,5 +1,5 @@
 <?
-	if (file_put_contents('posts', $_POST['postContent'], FILE_APPEND | LOCK_EX) == FALSE) {
+	if (file_put_contents('posts.txt', $_POST['postContent'], FILE_APPEND | LOCK_EX) == FALSE) {
 		echo 'Oops! Post failed.';
 	} else {
 		echo 'Post was successful.';
