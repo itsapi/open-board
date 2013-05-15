aad0d54c363878dbe2b388ad6490ad062ea5c9f0
grit96 <geraintwhite@gmail.com>
2013-05-15 19:34:47 +0100

    started the scrolling loading

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ function submitPost() {
 		}
 	});
 }
+$(window).scroll(function()
+{
+    if($(window).scrollTop() == $(document).height() - $(window).height())
+    {
+        $('div#loadmoreajaxloader').show();
+        $.ajax({
+        url: "viewPosts.php",
+        success: function(html)
+        {
+            if(html)
+            {
+                $("#board ul").append(html);
+                $('div#loadmoreajaxloader').hide();
+            }else
+            {
+                $('div#loadmoreajaxloader').html('<center>No more posts to show.</center>');
+            }
+        }
+        });
+    }
+});
