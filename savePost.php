7b61e490f613fc0d955b3cb77fba27a2a44e0ec2
grit96 <geraintwhite@gmail.com>
2013-05-15 17:27:10 +0100

    newline in submit post

--- savePost.php
+++ savePost.php
@@ -1,5 +1,8 @@
 <?
-	if (file_put_contents('posts', $_POST['postContent'], FILE_APPEND | LOCK_EX) == FALSE) {
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	if (file_put_contents('posts', $_POST['postContent'] . '\n', FILE_APPEND | LOCK_EX) == FALSE) {
 		echo 'Oops! Post failed.';
 	} else {
 		echo 'Post was successful.';
